d26e37d565f5b9c6bf82e408496534f914c39750
Enrique Rodriguez <enrique.rodriguez.garrido@gmail.com>
2020-12-18 21:26:07 -0300

    finish details

--- delete.sh
+++ delete.sh
@@ -1,2 +1,3 @@
 aws cloudformation delete-stack --stack-name $STACK_NAME --profile $PROFILE --region $REGION
-aws s3 rb --force s3://$DEPLOY_BUCKET
+aws s3 rb --force --profile $PROFILE s3://$DEPLOY_BUCKET 
+aws s3 ls --profile $PROFILE --region $REGION | grep transcribeviewer 
--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,9 +1,10 @@
 export DEPLOY_BUCKET=chime-transcribe-translate-comprehen-deployment-2
-export PROFILE=<your local aws profile here>
+export PROFILE=<your aws local profile>
 export STACK_NAME=chime-intelligent-meeting-backend
 export REGION=us-west-2
 
 aws s3 mb s3://$DEPLOY_BUCKET --region $REGION --profile $PROFILE
 sam deploy --stack-name $STACK_NAME --s3-bucket $DEPLOY_BUCKET --capabilities CAPABILITY_IAM --region $REGION --profile $PROFILE
 aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query 'Stacks[0].Outputs[?OutputKey==`ApiURL`].OutputValue' --output text --profile $PROFILE | read APIURL
-echo $APIURL
+echo "export const CHIME_BACKEND='$APIURL'" > ./src/chime-api-url.js
+cat ./src/chime-api-url.js
